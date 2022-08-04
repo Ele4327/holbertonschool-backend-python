039a0f8d32050a7ff9365e64a5bb53aefb7aa401
L <sebasgarzons88@gmail.com>
2022-08-04 16:59:56 -0500

    Fixed Documentation

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
-# Function takes two strings and returns a concantenated string
+"""Function takes two strings and returns a concantenated string"""
+
+
 def concat(str1: str, str2: str) -> str:
+    """Concatenate str 1 and str 2, and return in one string"""
     return (str1 + str2)
