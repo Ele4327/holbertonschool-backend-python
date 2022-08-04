88fd43de161d2ff4f55f59dbc82a4d08e8e713ce
L <sebasgarzons88@gmail.com>
2022-08-04 17:09:23 -0500

    Fixed Documentation Test without # V II

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
-""" Function which takes a float as argument and returns the floor of that number """
+""" Function which takes a float as argument and returns
+the floor of that number """
 
 
 def floor(n: float) -> int:
     """ Function floor takes n and return the floor
-        (low int number more aprox from the number passed) of the float."""
+    (low int number more aprox from the number passed) of the float."""
     return int(n)
