52ae1a277041c5ab8dd8de88fe51d568b2e8c500
L <sebasgarzons88@gmail.com>
2022-08-04 17:13:46 -0500

    Fixed Documentation - float to string

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
-# Function takes a float element as argument and return the same element
-# in string type
+""" Function takes a float element as argument and return the same element
+    in string type """
+
+
 def to_str(n: float) -> str:
+    """ to_str takes n and return the same element casted to string """
     return str(n)
