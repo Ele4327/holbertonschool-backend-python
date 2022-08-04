19cf4ec221a2f62e00cec94a44aef57881e4b1c0
L <sebasgarzons88@gmail.com>
2022-08-04 17:25:13 -0500

    Update fixed documentation

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Declaration and definition of typed-annotation variables
+"""  Declaration and definition of typed-annotation variables """
 a: int = 1
 pi: float = 3.14
 i_understand_annotations: bool = True
