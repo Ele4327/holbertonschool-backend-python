ef294072c2ff0e94dfbd8cef5a88a4f3b73f3dda
L <sebasgarzons88@gmail.com>
2022-08-04 19:27:51 -0500

    Update fixed documentation

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 
-from typing import Union, Tuple
+"""
+    A type-annotated function that takes a string (k) and an
+    int/float (v) as arguments and returns a tuple.
+    The second element should be annotated as a float.
+"""
 
-# A type-annotated function that takes a string (k) and an
-# int/float (v) as arguments and returns a tuple.
-# The second element should be annotated as a float.
+from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """ to_kv takes a string (k) and an int or float (v) and returns a tuple
+    with annotation especific for the second element."""
     return(k, (pow(v, 2)))
