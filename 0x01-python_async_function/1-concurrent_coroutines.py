175afad7590d788abe8602aeddb7a90822e386db
L <sebasgarzons88@gmail.com>
2022-08-08 15:22:14 -0500

    Multiple coroutines and order without sort

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+"""Function async that executes multiples coroutines concurrently"""
+
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """First executes multiple coroutines concurrently
+    and then returns all the values in a list, and then
+    organize that values in ascending order without sort"""
+    list_value = []
+    for z in range(n):
+        i = await wait_random(max_delay)
+        list_value.append(i)
+
+    new_list = []
+    while list_value:
+        min = list_value[0]
+        for x in list_value:
+            if x < min:
+                min = x
+        new_list.append(min)
+        list_value.remove(min)
+
+    print(new_list)
