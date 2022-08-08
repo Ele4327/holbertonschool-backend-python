4fee2f28b61aa1c6e72d5e69333fd5800a4beb6a
L <sebasgarzons88@gmail.com>
2022-08-08 04:57:53 -0500

    Return a sleep random

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""
+    Function asynchronous that waits a random number of seconds
+    between 0 and max_delay.
+"""
+
+import asyncio
+import random
+
+
+async def wait_random(max_delay=10):
+    """ Function asynchronous that waits a random number of seconds
+    between 0 and max_delay."""
+    x = random.uniform(0, max_delay)
+    await asyncio.sleep(x)
+
+    return x
