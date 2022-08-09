190e99d58b6e1fd93e35fea2ad678e8f742fc39b
L <sebasgarzons88@gmail.com>
2022-08-09 05:14:29 -0500

    Async generator, loop and give a random number

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Coroutine async, create generator that loops 10 times
+    and wait 1 second, then yield a random number
+"""
+import asyncio
+import random
+from typing import Generator
+
+
+async def async_generator() -> Generator:
+    """Loop 10 times, await 1 sec for each time and
+    yield a random number float between 0 and 10"""
+    for x in range(10):
+        await asyncio.sleep(1)
+        yield(random.uniform(0, 10))
